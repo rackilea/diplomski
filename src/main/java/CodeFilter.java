608db71e58fa2910b619165ca2805a921750b41b
Lea Rački <lea.racki@asseco-see.hr>
2020-05-20 00:58:29 +0200

    Separating each code fragment from database into its own file.

--- src/main/java/CodeFilter.java
+++ src/main/java/CodeFilter.java
@@ -3,6 +3,9 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -27,6 +30,8 @@ public class CodeFilter {
 
         Statement stmt;
 
+        long fragmentNumber = 0;
+
         try (Connection connection = HConnectionPool.getConnection()) {
             connection.setAutoCommit(false);
 
@@ -34,13 +39,16 @@ public class CodeFilter {
 
             String sql = "select * from answer where answer.id in (select question.acceptedAnswerId from question)";
             ResultSet rs = stmt.executeQuery(sql);
+
+
             while(rs.next()){
                 //Retrieve by column name
                 int id  = rs.getInt("id");
                 String body = rs.getString("body");
 
-                String code = geCodeFromBody(body);
-                Answer answer = new Answer(id, body, code);
+                fragmentNumber = geCodeFromBody(body, fragmentNumber);
+
+                /*Answer answer = new Answer(id, body, code);
 
                 answers.add(answer);
 
@@ -49,23 +57,54 @@ public class CodeFilter {
                     executorService.execute(() -> new AnswerRepository().updateBulk(list));
 
                     answers = new ArrayList<>();
-                }
+                }*/
             }
             rs.close();
 
-            if (answers.size() != 0) {
+            /*if (answers.size() != 0) {
                 final List<Answer> list = new ArrayList<>(answers);
                 executorService.execute(() -> new AnswerRepository().updateBulk(list));
-            }
+            }*/
         } catch (SQLException e) {
             System.err.println(e);
         }
 
         executorService.shutdown();
 
+        System.out.println(fragmentNumber);
+
     }
 
-    public static String geCodeFromBody(final String body) {
+    public static long geCodeFromBody(final String body, long fragmentNumber) {
+
+        Document doc = Jsoup.parse(body);
+        Elements link = doc.getElementsByTag("code");
+
+        for (Element element : link) {
+            String code = element.text();
+
+            if(code.split("\r\n|\r|\n").length < 6){
+                continue;
+            }
+
+            File input = new File("/Users/lracki/Desktop/diplomski/filtered2", "fragment_" + fragmentNumber++ + ".java");
+
+            try {
+                FileOutputStream out = new FileOutputStream(input);
+
+                out.write(code.getBytes());
+                out.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return fragmentNumber;
+    }
+
+    /*public static String geCodeFromBody(final String body) {
         StringBuilder code = new StringBuilder();
 
         Document doc = Jsoup.parse(body);
@@ -76,5 +115,5 @@ public class CodeFilter {
         }
 
         return code.toString();
-    }
+    }*/
 }
